b43f560853ac7df73b303dbe1c11c7562b963083
@mohammad_matin_parvin <mohammad123matin@gmail.com>
2021-05-02 18:09:20 +0430

    database added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 import controller.AppController;
+import model.database.DataBase;
+
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        DataBase dataBase = DataBase.getInstance();
+        dataBase.run();
         System.out.println("Yo-Gi-Oh!!!!");
         AppController.getInstance().run();
         System.out.println("Bye");
+        dataBase.close();
     }
 }
