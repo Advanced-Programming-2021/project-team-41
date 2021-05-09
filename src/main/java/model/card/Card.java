14293eafe31bd4f26f6f84fe5cd566dc0b6520ab
arezekhanari <79265076+arezekhanari@users.noreply.github.com>
2021-05-09 19:21:51 +0430

    Update Card.java
    update card (father class)

--- src/main/java/model/card/Card.java
+++ src/main/java/model/card/Card.java
@@ -1,43 +1,52 @@
 package model.card;
 
+import java.util.HashMap;
+
 public class Card {
-    int id;
+    int cardId;
     String name;
     String type;
-    int level;
-    String attribute;
-    String monsterType;
-    String cardType;
-    int attack;
-    int defence;
-    String property;
-    String status;
     String description;
     int price;
 
-    public Card(int id, String name, String type, int level, String attribute, String monsterType, String cardType
-            , int attack, int defence, String property, String status, String description, int price) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.level = level;
-        this.attribute = attribute;
-        this.monsterType = monsterType;
-        this.cardType = cardType;
-        this.attack = attack;
-        this.defence = defence;
-        this.property = property;
-        this.status = status;
-        this.description = description;
-        this.price = price;
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getId() {
-        return id;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 
     @Override
