d160076e45ea2bb4fa2469b80146fd772310b977
@mohammad_matin_parvin <mohammad123matin@gmail.com>
2021-05-10 02:18:02 +0430

    wallet class added

---
+++ src/main/java/model/user/Wallet.java
@@ -0,0 +1,39 @@
+package model.user;
+
+public class Wallet {
+    /* Instance Fields */
+    private final User user;
+    private int amount = 100000;
+
+    /* Constructor */
+    Wallet(User user) {
+        this.user = user;
+    }
+
+    Wallet(User user, int amount) {
+        this.user = user;
+        this.amount = amount;
+    }
+
+    /* Getters And Setters */
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        UserDB.updateMoney(this.user.getId(), amount);
+        this.amount = amount;
+    }
+
+    /* Instance Methods */
+    public void increaseAmount(int amount) throws WalletException {
+        if (amount < 0) throw new NegativeIncreaseValue();
+        setAmount(getAmount() + amount);
+    }
+
+    public void decreaseAmount(int amount) throws WalletException {
+        if (amount < 0) amount = -amount;
+        if (amount > this.amount) throw new NotEnoughMoney();
+        setAmount(getAmount() - amount);
+    }
+}
