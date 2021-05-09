ed0e77676574ce6b193d3605bda0d93513d90499
@mohammad_matin_parvin <mohammad123matin@gmail.com>
2021-05-10 01:42:15 +0430

    some features added

--- src/main/java/model/database/user/UserDB.java
+++ src/main/java/model/database/user/UserDB.java
@@ -8,20 +8,30 @@ public class UserDB {
 
     /* Static methods */
     public static int countUserByNickname(String nickname) {
-        return dataBase.getCount(String.format(Queries.GET_USER_BY_NICKNAME.getValue(), nickname));
+        return dataBase.getCount(String.format(Queries.GET_USER_BY_NICKNAME.getQuery(), nickname));
     }
 
     public static int countUserByUsername(String username) {
-        return dataBase.getCount(String.format(Queries.GET_USER_BY_USERNAME.getValue(), username));
+        return dataBase.getCount(String.format(Queries.GET_USER_BY_USERNAME.getQuery(), username));
     }
 
     public static int countUserById(int id) {
-        return dataBase.getCount(String.format(Queries.GET_USER_BY_ID.getValue(), id));
+        return dataBase.getCount(String.format(Queries.GET_USER_BY_ID.getQuery(), id));
     }
 
     public static int insertUser(String username, String nickname, String password, int score, int money) {
-        dataBase.exeUpdate(String.format(Queries.INSERT_USER.getValue(), username, nickname, password, score, money));
+        dataBase.exeUpdate(String.format(Queries.INSERT_USER.getQuery(), username, nickname, password, score, money));
         return dataBase.getGenerateKey();
+    }
+
+    public static void updateMoney(int userId, int newMoney) throws UserDoesNotExists {
+        userIdExistsInDatabase(userId);
+        dataBase.exeUpdate(String.format(Queries.UPDATE_USER_MONEY.getQuery(), newMoney, userId));
+    }
+
+    public static void updateScore(int userId, int newScore) throws UserDoesNotExists {
+        userIdExistsInDatabase(userId);
+        dataBase.exeUpdate(String.format(Queries.UPDATE_USER_SCORE.getQuery(), newScore, userId));
     }
 
     public static void userIdExistsInDatabase(int userId) throws UserDoesNotExists {
