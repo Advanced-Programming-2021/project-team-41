0010c0ebb14337f58152502cca8230957913f984
arezekhanari <79265076+arezekhanari@users.noreply.github.com>
2021-05-11 13:47:12 +0430

    update card class, transfer toSting function to Monster class and create card type(normal and effective)

--- src/main/java/model/card/Card.java
+++ src/main/java/model/card/Card.java
@@ -3,7 +3,6 @@ package model.card;
 public class Card {
     private int cardId;
     private String name;
-    private String type;
     private String description;
     private int price;
 
@@ -23,14 +22,6 @@ public class Card {
         this.name = name;
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
     public String getDescription() {
         return description;
     }
@@ -46,23 +37,4 @@ public class Card {
     public void setPrice(int price) {
         this.price = price;
     }
-
-//    @Override
-//    public String toString() {
-//        return "Card{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", type='" + type + '\'' +
-//                ", level=" + level +
-//                ", attribute='" + attribute + '\'' +
-//                ", monsterType='" + monsterType + '\'' +
-//                ", cardType='" + cardType + '\'' +
-//                ", attack=" + attack +
-//                ", defence=" + defence +
-//                ", property='" + property + '\'' +
-//                ", status='" + status + '\'' +
-//                ", description='" + description + '\'' +
-//                ", price=" + price +
-//                '}';
-//    }
 }
