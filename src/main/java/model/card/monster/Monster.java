2d93df516b59caf5e883ab4e3f5312b622cf06b2
arezekhanari <79265076+arezekhanari@users.noreply.github.com>
2021-05-11 13:50:02 +0430

    update Monster class, update setMonsterType and setAttribute function

--- src/main/java/model/card/monster/Monster.java
+++ src/main/java/model/card/monster/Monster.java
@@ -6,6 +6,7 @@ public class Monster extends Card {
     private int level;
     private Attribute attribute;
     private MonsterType monsterType;
+    private CardType cardType;
     private int attack;
     private int defence;
 
@@ -21,16 +22,64 @@ public class Monster extends Card {
         return attribute;
     }
 
-    public void setAttribute(Attribute attribute) {
-        this.attribute = attribute;
+    public void setAttribute(String attribute) {
+        if ("EARTH".equals(attribute)) {
+            this.attribute = Attribute.EARTH;
+        } else if ("WATER".equals(attribute)) {
+            this.attribute = Attribute.WATER;
+        } else if ("DARK".equals(attribute)) {
+            this.attribute = Attribute.DARK;
+        } else if ("FIRE".equals(attribute)) {
+            this.attribute = Attribute.FIRE;
+        } else if ("LIGHT".equals(attribute)) {
+            this.attribute = Attribute.LIGHT;
+        } else if ("WIND".equals(attribute)) {
+            this.attribute = Attribute.WIND;
+        }
     }
 
     public MonsterType getMonsterType() {
         return monsterType;
     }
 
-    public void setMonsterType(MonsterType monsterType) {
-        this.monsterType = monsterType;
+    public void setMonsterType(String monsterType) {
+        if ("BEAST_WARRIOR".equals(monsterType)) {
+            this.monsterType = MonsterType.BEAST_WARRIOR;
+        } else if ("WARRIOR".equals(monsterType)) {
+            this.monsterType = MonsterType.WARRIOR;
+        } else if ("AQUA".equals(monsterType)) {
+            this.monsterType = MonsterType.AQUA;
+        } else if ("FIEND".equals(monsterType)) {
+            this.monsterType = MonsterType.FIEND;
+        } else if ("BEAST".equals(monsterType)) {
+            this.monsterType = MonsterType.BEAST;
+        } else if ("PYRO".equals(monsterType)) {
+            this.monsterType = MonsterType.PYRO;
+        } else if ("SPELL_CASTER".equals(monsterType)) {
+            this.monsterType = MonsterType.SPELL_CASTER;
+        } else if ("THUNDER".equals(monsterType)) {
+            this.monsterType = MonsterType.THUNDER;
+        } else if ("MACHINE".equals(monsterType)) {
+            this.monsterType = MonsterType.MACHINE;
+        } else if ("ROCK".equals(monsterType)) {
+            this.monsterType = MonsterType.ROCK;
+        } else if ("DRAGON".equals(monsterType)) {
+            this.monsterType = MonsterType.DRAGON;
+        } else if ("INSECT".equals(monsterType)) {
+            this.monsterType = MonsterType.INSECT;
+        } else if ("CYBERSE".equals(monsterType)) {
+            this.monsterType = MonsterType.CYBERSE;
+        } else if ("FAIRY".equals(monsterType)) {
+            this.monsterType = MonsterType.FAIRY;
+        }
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(CardType cardType) {
+        this.cardType = cardType;
     }
 
     public int getAttack() {
@@ -66,5 +115,24 @@ public class Monster extends Card {
 
     public void decreaseAttack(int amount) {
         attack -= amount;
+    }
+//    -----------------------------------------------------------------------
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "id=" + getCardId() +
+                ", name='" + getName() + '\'' +
+                ", type='" + getCardType() + '\'' +
+                ", level=" + level +
+                ", attribute='" + attribute + '\'' +
+                ", monsterType='" + monsterType + '\'' +
+                ", cardType='" + cardType + '\'' +
+                ", attack=" + attack +
+                ", defence=" + defence +
+                ", property='" + property + '\'' +
+                ", status='" + status + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", price=" + getPrice() +
+                '}';
     }
 }
