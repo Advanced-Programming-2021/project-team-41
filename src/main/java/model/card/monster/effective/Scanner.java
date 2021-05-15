9f87b44548e69be8fbc3f7ac1c8247136f305f35
arezekhanari <79265076+arezekhanari@users.noreply.github.com>
2021-05-15 18:42:00 +0430

    update Scanner.java

--- src/main/java/model/card/monster/effective/Scanner.java
+++ src/main/java/model/card/monster/effective/Scanner.java
@@ -1,2 +1,40 @@
-package model.card.monster.effective;public class Scanner {
+package model.card.monster.effective;
+
+import model.card.Card;
+import model.card.monster.CardType;
+import model.card.monster.Monster;
+
+public class Scanner extends Monster {
+    private boolean isAppliedEffectInThisTurn = false;
+    private Card scannedCard = null;
+
+    public Scanner(String name, int level, String attribute, String monsterType, int attack, int defence, String description, int price) {
+        setName(name);
+        setLevel(level);
+        setAttribute(attribute);
+        setMonsterType(monsterType);
+        setCardType(CardType.EFFECT);
+        setAttack(attack);
+        setDefence(defence);
+        setDescription(description);
+        setPrice(price);
+    }
+
+//    after being placed on the board, it becomes the scanned card :
+    public void applyEffect(Card scannedCard) {
+        setScannedCard(scannedCard);
+    }
+
+    public Card getScannedCard() {
+        return scannedCard;
+    }
+
+    public void setScannedCard(Card scannedCard) {
+        this.scannedCard = scannedCard;
+    }
+
+//    before start new turn
+    public void disableEffect() {
+        setScannedCard(null);
+    }
 }
