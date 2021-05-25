352eeef4380745f6aaa15199b5a39f8809dacc7f
arezekhanari <79265076+arezekhanari@users.noreply.github.com>
2021-05-25 11:47:14 +0430

    continuous spell cards, packaging

--- src/main/java/model/card/spellAndTrap/spell/continuos/SpellAbsorption.java
+++ src/main/java/model/card/spellAndTrap/spell/continuos/SpellAbsorption.java
@@ -1,8 +1,9 @@
-package model.card.spellAndTrap.spell;
+package model.card.spellAndTrap.spell.continuos;
 
 import model.card.spellAndTrap.Icon;
+import model.card.spellAndTrap.spell.Spell;
 
-public class SpellAbsorption extends Spell{
+public class SpellAbsorption extends Spell {
     public SpellAbsorption(String name, String description, int price) {
 
         super(name, Icon.CONTINUOUS, description, Status.UNLIMITED, price);
--- src/main/java/model/card/spellAndTrap/spell/continuos/SupplySquad.java
+++ src/main/java/model/card/spellAndTrap/spell/continuos/SupplySquad.java
@@ -1,8 +1,9 @@
-package model.card.spellAndTrap.spell;
+package model.card.spellAndTrap.spell.continuos;
 
 import model.card.spellAndTrap.Icon;
+import model.card.spellAndTrap.spell.Spell;
 
-public class SupplySquad extends Spell{
+public class SupplySquad extends Spell {
     public SupplySquad(String name, String description, int price) {
 
         super(name, Icon.CONTINUOUS, description, Status.UNLIMITED, price);
