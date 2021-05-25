7a0b8740bc3f8f8d489e401b97d90abf506fbcd0
@mohammad_matin_parvin <mohammad123matin@gmail.com>
2021-05-25 10:46:34 +0430

    new structure done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,8 @@
-import controller.AppController;
-import model.database.DataBase;
-
-import java.sql.SQLException;
+import controller.App;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        DataBase dataBase = DataBase.getInstance();
-        dataBase.run();
-        System.out.println("Yo-Gi-Oh!!!!");
-        AppController.getInstance().run();
-        System.out.println("Bye");
-        dataBase.close();
+    /* Static methods */
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        App.getInstance().run();
     }
 }
