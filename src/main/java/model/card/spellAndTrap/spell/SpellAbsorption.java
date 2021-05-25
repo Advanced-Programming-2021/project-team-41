288806d9ab5b9e501ee266be1bca179bed233870
arezekhanari <79265076+arezekhanari@users.noreply.github.com>
2021-05-25 11:35:20 +0430

    update Spell Absorption spell card

--- src/main/java/model/card/spellAndTrap/spell/SpellAbsorption.java
+++ src/main/java/model/card/spellAndTrap/spell/SpellAbsorption.java
@@ -5,7 +5,7 @@ import model.card.spellAndTrap.Icon;
 public class SpellAbsorption extends Spell{
     public SpellAbsorption(String name, String description, int price) {
 
-        super(name, Icon.NORMAL, description, Status.UNLIMITED, price);
+        super(name, Icon.CONTINUOUS, description, Status.UNLIMITED, price);
 
     }
 
